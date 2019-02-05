c4ef0381f06fc3ac2d400420add46eafa9b6250f
Layla Gammon <WS311472@weston.ac.uk>
2019-02-05 14:12:32 +0000

    Initial Commit

--- Structures/Structures.cpp
+++ Structures/Structures.cpp
@@ -1,21 +1,113 @@
-// Structures.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+struct User
+{
+	string sName;
+	int iAge;
+	float fHeight;
+	int iNumbers[5];
+
+};
+
+void PopulateDetails(User *objUserPass, const int *iSizePass)
+{
+	for (int iCount = 0; iCount < *iSizePass; iCount++)
+	{
+		cout << "Please enter the name of the user\n";
+		cin >> objUserPass[iCount].sName;
+
+		cout << "\nPlease enter your age " << objUserPass[iCount].sName;
+		cin >> objUserPass[iCount].iAge;
+
+		cout << "\nPlease enter your Height " << objUserPass[iCount].sName;
+		cin >> objUserPass[iCount].fHeight;
+	}
+}
+
+void OutputDetails(User *objUserPass, const int *iSizePass)
+{
+	for (int iCount = 0; iCount < *iSizePass; iCount++)
+	{
+		cout << "Name: " << objUserPass[iCount].sName
+			<< "\nAge: " << objUserPass[iCount].iAge
+			<< "\nHeight: " << objUserPass[iCount].fHeight;
+	}
+}
+
+void UpdateNumberForAllUsers(User *objUserPass, const int *iSizePass)
+{
+	for (int iCount = 0; iCount < *iSizePass; iCount++)
+	{
+		cout << "Please enter the numbers for " << objUserPass[iCount].sName;
+
+		for (int iCount2 = 0; iCount2 < 5; iCount2++)
+		{
+			cout << "\nPlease enter number " << iCount2 + 1;
+			cin >> objUserPass[iCount].iNumbers[iCount2];
+		}
+
+	}
+
+}
+
+void CalculateTotalByUser(User *objUserPass, const int *iSize)
+{
+	int iTotal = 0;
+
+	for (int iCount1 = 0; iCount1 < *iSize; iCount1++)
+	{
+		cout << "\nCalculating the total for: " << objUserPass[iCount1].sName;
+
+		for (int iCount2 = 0; iCount2 < 5; iCount2++)
+		{
+			iTotal += objUserPass[iCount1].iNumbers[iCount2];
+		}
+
+		cout << "\nThe total is: " << iTotal;
+	}
+}
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+
+	const int iSize = 3;
+	User objUser[iSize];
+
+	PopulateDetails(objUser, &iSize);
+	OutputDetails(objUser, &iSize);
+
+	_getch();
+
+
+	/*for (int iCount = 0; iCount < iSize; iCount++)
+	{
+	cout << "Please enter the users name \n";
+	cin >> objUser[iCount].sName;
+
+	cout << "Please enter your age " << objUser[iCount].sName;
+	cin >> objUser[iCount].iAge;
+
+	cout << "Please enter your height" << objUser[iCount].sName;
+	cin >> objUser[iCount].fHeight;
+
+	}*/
+
+	/*cout << "Welcome to the program\n";
+
+	cout << "Please enter the name of the the user\n";
+	cin >> objUser.sName;
+
+	cout << "\nPlease enter your age " << objUser.sName;
+	cin >> objUser.iAge;
+
+	cout << "\nPlease enter your height \n";
+	cin >> objUser.fHeight;*/
+
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
